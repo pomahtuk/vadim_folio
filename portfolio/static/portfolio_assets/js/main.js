0d8e7ebf6727543272a95bb285b31cff16a25e9f
pomahtuk <pman89@ya.ru>
2016-06-06 00:15:57 +0200

    some assaets defenition

--- portfolio/static/portfolio_assets/js/main.js
+++ portfolio/static/portfolio_assets/js/main.js
@@ -1,3 +1,10 @@
 $(function () {
-   //console.log(1);
+  
+  var slideout = new Slideout({
+    'panel': document.getElementById('panel'),
+    'menu': document.getElementById('menu'),
+    'padding': 256,
+    'tolerance': 70
+  });
+
 });
