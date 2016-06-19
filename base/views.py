cec7feabaad3063615ade058c9a52b54d9683f58
pomahtuk <pman89@ya.ru>
2016-06-19 22:53:41 +0200

    ~~~

--- base/views.py
+++ base/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import SettingsObject
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    settings_data = SettingsObject.objects.all()[:1]
+
+    return render(request, 'index.html', {
+        'settings': settings_data
+    })
--- dump.sql
+++ dump.sql
@@ -84,7 +84,7 @@ CREATE TABLE `auth_permission` (
   UNIQUE KEY `auth_permission_content_type_id_01ab375a_uniq` (`content_type_id`,`codename`),
   KEY `auth_permission_417f1b1c` (`content_type_id`),
   CONSTRAINT `auth_permissi_content_type_id_2f476e4b_fk_django_content_type_id` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -93,7 +93,7 @@ CREATE TABLE `auth_permission` (
 
 LOCK TABLES `auth_permission` WRITE;
 /*!40000 ALTER TABLE `auth_permission` DISABLE KEYS */;
-INSERT INTO `auth_permission` VALUES (1,'Can add log entry',1,'add_logentry'),(2,'Can change log entry',1,'change_logentry'),(3,'Can delete log entry',1,'delete_logentry'),(4,'Can add permission',2,'add_permission'),(5,'Can change permission',2,'change_permission'),(6,'Can delete permission',2,'delete_permission'),(7,'Can add group',3,'add_group'),(8,'Can change group',3,'change_group'),(9,'Can delete group',3,'delete_group'),(10,'Can add user',4,'add_user'),(11,'Can change user',4,'change_user'),(12,'Can delete user',4,'delete_user'),(13,'Can add content type',5,'add_contenttype'),(14,'Can change content type',5,'change_contenttype'),(15,'Can delete content type',5,'delete_contenttype'),(16,'Can add session',6,'add_session'),(17,'Can change session',6,'change_session'),(18,'Can delete session',6,'delete_session'),(19,'Can add Новость',7,'add_news'),(20,'Can change Новость',7,'change_news'),(21,'Can delete Новость',7,'delete_news'),(22,'Can add Страница',8,'add_page'),(23,'Can change Страница',8,'change_page'),(24,'Can delete Страница',8,'delete_page'),(25,'Can add kv store',9,'add_kvstore'),(26,'Can change kv store',9,'change_kvstore'),(27,'Can delete kv store',9,'delete_kvstore');
+INSERT INTO `auth_permission` VALUES (1,'Can add log entry',1,'add_logentry'),(2,'Can change log entry',1,'change_logentry'),(3,'Can delete log entry',1,'delete_logentry'),(4,'Can add permission',2,'add_permission'),(5,'Can change permission',2,'change_permission'),(6,'Can delete permission',2,'delete_permission'),(7,'Can add group',3,'add_group'),(8,'Can change group',3,'change_group'),(9,'Can delete group',3,'delete_group'),(10,'Can add user',4,'add_user'),(11,'Can change user',4,'change_user'),(12,'Can delete user',4,'delete_user'),(13,'Can add content type',5,'add_contenttype'),(14,'Can change content type',5,'change_contenttype'),(15,'Can delete content type',5,'delete_contenttype'),(16,'Can add session',6,'add_session'),(17,'Can change session',6,'change_session'),(18,'Can delete session',6,'delete_session'),(19,'Can add Новость',7,'add_news'),(20,'Can change Новость',7,'change_news'),(21,'Can delete Новость',7,'delete_news'),(22,'Can add Страница',8,'add_page'),(23,'Can change Страница',8,'change_page'),(24,'Can delete Страница',8,'delete_page'),(25,'Can add kv store',9,'add_kvstore'),(26,'Can change kv store',9,'change_kvstore'),(27,'Can delete kv store',9,'delete_kvstore'),(28,'Can add Проект',10,'add_work'),(29,'Can change Проект',10,'change_work'),(30,'Can delete Проект',10,'delete_work'),(31,'Can add Подробность',11,'add_workpart'),(32,'Can change Подробность',11,'change_workpart'),(33,'Can delete Подробность',11,'delete_workpart'),(34,'Can add Настройки',12,'add_settingsobject'),(35,'Can change Настройки',12,'change_settingsobject'),(36,'Can delete Настройки',12,'delete_settingsobject');
 /*!40000 ALTER TABLE `auth_permission` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -230,6 +230,37 @@ INSERT INTO `base_page` VALUES (1,'Главная','index','1',1,1,1,2,1,0,NULL,
 UNLOCK TABLES;
 
 --
+-- Table structure for table `base_settingsobject`
+--
+
+DROP TABLE IF EXISTS `base_settingsobject`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `base_settingsobject` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fb_url` varchar(200) NOT NULL,
+  `insta_url` varchar(200) NOT NULL,
+  `vk_url` varchar(200) NOT NULL,
+  `youtube_url` varchar(200) NOT NULL,
+  `twitter_url` varchar(200) NOT NULL,
+  `phone` varchar(200) NOT NULL,
+  `address` varchar(200) NOT NULL,
+  `mail` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `base_settingsobject`
+--
+
+LOCK TABLES `base_settingsobject` WRITE;
+/*!40000 ALTER TABLE `base_settingsobject` DISABLE KEYS */;
+INSERT INTO `base_settingsobject` VALUES (1,'1','2','3','4','5','6','7','8');
+/*!40000 ALTER TABLE `base_settingsobject` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `django_admin_log`
 --
 
@@ -250,7 +281,7 @@ CREATE TABLE `django_admin_log` (
   KEY `django_admin_log_e8701ad4` (`user_id`),
   CONSTRAINT `django_admin__content_type_id_c4bce8eb_fk_django_content_type_id` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`),
   CONSTRAINT `django_admin_log_user_id_c564eba6_fk_auth_user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -259,7 +290,7 @@ CREATE TABLE `django_admin_log` (
 
 LOCK TABLES `django_admin_log` WRITE;
 /*!40000 ALTER TABLE `django_admin_log` DISABLE KEYS */;
-INSERT INTO `django_admin_log` VALUES (1,'2016-06-09 20:58:33.943151','1','Главная',1,'Добавлено.',8,1),(2,'2016-06-09 20:58:53.217924','2','Проекты',1,'Добавлено.',8,1),(3,'2016-06-09 20:59:15.701735','3','Профиль',1,'Добавлено.',8,1),(4,'2016-06-09 20:59:30.715099','4','Дизайн',1,'Добавлено.',8,1),(5,'2016-06-09 20:59:47.049659','5','Архитектура',1,'Добавлено.',8,1),(6,'2016-06-09 21:00:04.432418','6','Интерьер',1,'Добавлено.',8,1),(7,'2016-06-09 21:00:24.800892','7','Рекомендации',1,'Добавлено.',8,1),(8,'2016-06-09 21:00:44.874767','8','Новости',1,'Добавлено.',8,1),(9,'2016-06-09 21:01:03.362953','9','Контакты',1,'Добавлено.',8,1);
+INSERT INTO `django_admin_log` VALUES (1,'2016-06-09 20:58:33.943151','1','Главная',1,'Добавлено.',8,1),(2,'2016-06-09 20:58:53.217924','2','Проекты',1,'Добавлено.',8,1),(3,'2016-06-09 20:59:15.701735','3','Профиль',1,'Добавлено.',8,1),(4,'2016-06-09 20:59:30.715099','4','Дизайн',1,'Добавлено.',8,1),(5,'2016-06-09 20:59:47.049659','5','Архитектура',1,'Добавлено.',8,1),(6,'2016-06-09 21:00:04.432418','6','Интерьер',1,'Добавлено.',8,1),(7,'2016-06-09 21:00:24.800892','7','Рекомендации',1,'Добавлено.',8,1),(8,'2016-06-09 21:00:44.874767','8','Новости',1,'Добавлено.',8,1),(9,'2016-06-09 21:01:03.362953','9','Контакты',1,'Добавлено.',8,1),(10,'2016-06-14 19:40:43.112967','1','8',1,'Добавлено.',12,1);
 /*!40000 ALTER TABLE `django_admin_log` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -276,7 +307,7 @@ CREATE TABLE `django_content_type` (
   `model` varchar(100) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `django_content_type_app_label_76bd3d3b_uniq` (`app_label`,`model`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -285,7 +316,7 @@ CREATE TABLE `django_content_type` (
 
 LOCK TABLES `django_content_type` WRITE;
 /*!40000 ALTER TABLE `django_content_type` DISABLE KEYS */;
-INSERT INTO `django_content_type` VALUES (1,'admin','logentry'),(3,'auth','group'),(2,'auth','permission'),(4,'auth','user'),(8,'base','page'),(5,'contenttypes','contenttype'),(7,'news','news'),(6,'sessions','session'),(9,'thumbnail','kvstore');
+INSERT INTO `django_content_type` VALUES (1,'admin','logentry'),(3,'auth','group'),(2,'auth','permission'),(4,'auth','user'),(8,'base','page'),(12,'base','settingsobject'),(5,'contenttypes','contenttype'),(7,'news','news'),(6,'sessions','session'),(9,'thumbnail','kvstore'),(10,'works','work'),(11,'works','workpart');
 /*!40000 ALTER TABLE `django_content_type` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -302,7 +333,7 @@ CREATE TABLE `django_migrations` (
   `name` varchar(255) NOT NULL,
   `applied` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -311,7 +342,7 @@ CREATE TABLE `django_migrations` (
 
 LOCK TABLES `django_migrations` WRITE;
 /*!40000 ALTER TABLE `django_migrations` DISABLE KEYS */;
-INSERT INTO `django_migrations` VALUES (1,'contenttypes','0001_initial','2016-06-02 09:09:13'),(2,'auth','0001_initial','2016-06-02 09:09:13'),(3,'admin','0001_initial','2016-06-02 09:09:13'),(4,'admin','0002_logentry_remove_auto_add','2016-06-02 09:09:13'),(5,'contenttypes','0002_remove_content_type_name','2016-06-02 09:09:13'),(6,'auth','0002_alter_permission_name_max_length','2016-06-02 09:09:13'),(7,'auth','0003_alter_user_email_max_length','2016-06-02 09:09:13'),(8,'auth','0004_alter_user_username_opts','2016-06-02 09:09:13'),(9,'auth','0005_alter_user_last_login_null','2016-06-02 09:09:13'),(10,'auth','0006_require_contenttypes_0002','2016-06-02 09:09:13'),(11,'auth','0007_alter_validators_add_error_messages','2016-06-02 09:09:13'),(12,'sessions','0001_initial','2016-06-02 09:09:13'),(13,'news','0001_initial','2016-06-02 09:33:34'),(14,'news','0002_auto_20160603_1447','2016-06-03 14:47:47'),(15,'base','0001_initial','2016-06-09 19:54:25'),(16,'news','0003_auto_20160609_1954','2016-06-09 19:54:25'),(17,'base','0002_auto_20160609_2043','2016-06-09 20:43:37');
+INSERT INTO `django_migrations` VALUES (1,'contenttypes','0001_initial','2016-06-02 09:09:13'),(2,'auth','0001_initial','2016-06-02 09:09:13'),(3,'admin','0001_initial','2016-06-02 09:09:13'),(4,'admin','0002_logentry_remove_auto_add','2016-06-02 09:09:13'),(5,'contenttypes','0002_remove_content_type_name','2016-06-02 09:09:13'),(6,'auth','0002_alter_permission_name_max_length','2016-06-02 09:09:13'),(7,'auth','0003_alter_user_email_max_length','2016-06-02 09:09:13'),(8,'auth','0004_alter_user_username_opts','2016-06-02 09:09:13'),(9,'auth','0005_alter_user_last_login_null','2016-06-02 09:09:13'),(10,'auth','0006_require_contenttypes_0002','2016-06-02 09:09:13'),(11,'auth','0007_alter_validators_add_error_messages','2016-06-02 09:09:13'),(12,'sessions','0001_initial','2016-06-02 09:09:13'),(13,'news','0001_initial','2016-06-02 09:33:34'),(14,'news','0002_auto_20160603_1447','2016-06-03 14:47:47'),(15,'base','0001_initial','2016-06-09 19:54:25'),(16,'news','0003_auto_20160609_1954','2016-06-09 19:54:25'),(17,'base','0002_auto_20160609_2043','2016-06-09 20:43:37'),(18,'base','0003_settingsobject','2016-06-14 19:38:34'),(19,'works','0001_initial','2016-06-14 19:38:34'),(20,'works','0002_auto_20160614_1938','2016-06-14 19:38:34');
 /*!40000 ALTER TABLE `django_migrations` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -366,6 +397,59 @@ LOCK TABLES `news_news` WRITE;
 /*!40000 ALTER TABLE `news_news` DISABLE KEYS */;
 /*!40000 ALTER TABLE `news_news` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `works_work`
+--
+
+DROP TABLE IF EXISTS `works_work`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `works_work` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `works_work`
+--
+
+LOCK TABLES `works_work` WRITE;
+/*!40000 ALTER TABLE `works_work` DISABLE KEYS */;
+/*!40000 ALTER TABLE `works_work` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `works_workpart`
+--
+
+DROP TABLE IF EXISTS `works_workpart`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `works_workpart` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(200) NOT NULL,
+  `content` longtext NOT NULL,
+  `use_video` tinyint(1) NOT NULL,
+  `image` varchar(100) DEFAULT NULL,
+  `video` varchar(200) DEFAULT NULL,
+  `work_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `works_workpart_work_id_b1a6e25b_fk_works_work_id` (`work_id`),
+  CONSTRAINT `works_workpart_work_id_b1a6e25b_fk_works_work_id` FOREIGN KEY (`work_id`) REFERENCES `works_work` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `works_workpart`
+--
+
+LOCK TABLES `works_workpart` WRITE;
+/*!40000 ALTER TABLE `works_workpart` DISABLE KEYS */;
+/*!40000 ALTER TABLE `works_workpart` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -376,4 +460,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-06-09 23:01:27
+-- Dump completed on 2016-06-19 22:53:16
