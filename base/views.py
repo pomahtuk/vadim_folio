51f256690dc1edfddfcdccd7ee09f27ffb97872f
Ilya Lovriakov <ilya.lovriakov@booking.com>
2016-06-20 17:22:39 +0200

    passing pages from admin side, some template logic

--- base/views.py
+++ base/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from .models import SettingsObject
+from .models import SettingsObject, Page
 
 
 # Create your views here.
 def index(request):
     settings_data = SettingsObject.objects.all()[:1]
+    pages = Page.objects.all()
 
     return render(request, 'index.html', {
-        'settings': settings_data[0]
+        'settings': settings_data[0],
+        'pages': pages,
     })
